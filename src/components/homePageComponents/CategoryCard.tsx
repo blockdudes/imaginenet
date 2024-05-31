5c83f6e1e0b2112754a779c23fb9279bcf8ddc94
Pratham Mahajan <mahajanpratham@gmail.com>
2024-05-31 17:34:53 +0530

    commit

--- src/components/homePageComponents/CategoryCard.tsx
+++ src/components/homePageComponents/CategoryCard.tsx
@@ -6,7 +6,7 @@ const CategoryCard = ({ name, imageUrl, price, cid }: Game) => {
   const navigate = useNavigate();
   return (
     <div
-      className=" rounded-t-xl flex flex-col shadow-sm shadow-black cursor-pointer relative"
+      className="rounded-xl overflow-clip flex flex-col shadow-sm shadow-black cursor-pointer relative"
       onClick={() => navigate(`/buy/${cid}`)}
     >
       <div className="w-[50x] h-[20px] flex items-center rounded-md text-xs px-3 py-3 absolute text-white font-semibold  top-2 left-2 bg-[#32323b]">
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,6 +10,8 @@ const HomePage = () => {
     allGames,
     homePageLoader,
     handleGetGames,
+    signer,
+    address,
   } = useContext(GlobalContext)!;
 
   const handleWalletConnect = async () => {
@@ -20,30 +22,25 @@ const HomePage = () => {
     handleGetGames();
   }, [contractInstance]);
 
-  useEffect(() => {
-    walletConnect();
-  }, []);
-
   return (
     <div className="flex w-full">
       {/* <Sidebar /> */}
 
       <div className="flex-grow bg-blue">
-        {/* <div
-          className="fixed right-2 top-2 text-white"
-          onClick={() => handleWalletConnect()}
-        >
-          Connect Wallet
-        </div> */}
-        <button
-          className="fixed right-4 top-4 bg-secondary font-semibold text-primary-text rounded-lg px-5 py-2 "
-          onClick={handleWalletConnect}
-        >
-          Connect Wallet
-        </button>
-        {/* <button className="bg-white" onClick={() => uploadGame()}>
-          uploadGame
-        </button> */}
+        <div className="fixed right-4 top-4 bg-secondary font-semibold text-primary-text rounded-lg">
+          {signer != null ? (
+            <button className="w-[200px] px-5 py-2">
+              <p className="truncate text-secondary-text">{address}</p>
+            </button>
+          ) : (
+            <button
+              className="w-[200px] px-5 py-2"
+              onClick={handleWalletConnect}
+            >
+              Connect Wallet
+            </button>
+          )}
+        </div>
         <h1 className="ml-4 text-primary mt-12 font-semibold text-2xl">
           Market Place
         </h1>
